1b9abadeef9980c77e4e027f61d891ebe92040db
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-11-17 02:33:06 +0000

    Fixed some designs

--- src/apps/habits/habitsPage.js
+++ src/apps/habits/habitsPage.js
@@ -7,20 +7,39 @@ const HabitsPage = () => {
         {id: 1, title: 'Exercise', description: 'Do a 30-minute workout.'},
         {id: 2, title: 'Meditate', description: 'Practice for 10 minutes.'},
         {id: 3, title: 'Read', description: 'Read at least 10 Pages of a book.'},
+        {id: 1, title: 'Exercise', description: 'Do a 30-minute workout.'},
+        {id: 2, title: 'Meditate', description: 'Practice for 10 minutes.'},
+        {id: 3, title: 'Read', description: 'Read at least 10 Pages of a book.'},
+        {id: 1, title: 'Exercise', description: 'Do a 30-minute workout.'},
+        {id: 2, title: 'Meditate', description: 'Practice for 10 minutes.'},
+        {id: 3, title: 'Read', description: 'Read at least 10 Pages of a book.'},
+        // {id: 1, title: 'Exercise', description: 'Do a 30-minute workout.'},
+        // {id: 2, title: 'Meditate', description: 'Practice for 10 minutes.'},
+        // {id: 3, title: 'Read', description: 'Read at least 10 Pages of a book.'},
     ];
+
+
     return (
         <div className="habits-page">
-            <header>
-                <h2>{currentDate}</h2>
+             <header className="text-center text-date p-5">
+                <h2>{currentDate}</h2> {/* Centered date */}
             </header>
 
-            <section>
-                {habits.map((habit) => (
-                    <HabitCard key={habit.id} title={habit.title} description={habit.description} />
-                ))}
-            </section>
+            <div className="cards">
+                {habits.map((habit) => {
+                    return (
+                    <div className="card">
+                        <div className="card-body d-flex flex-column">
+                            <h5 className="card-title">{habit.title}</h5>
+                            <p className="card-text">{habit.description}</p>
+                        </div>
+                    </div>
+                )})}
+            </div>
+
         </div>
-    )
+    );
 }
+
 
 export default HabitsPage;
